4bd11a90a469e75d6b9240070c479a562318b520
wayllertt <alidzhieva2005@mail.ru>
2025-11-12 00:35:02 +0300

    иьтkm

--- app/src/main/java/com/example/playlist_maker_android_rassohinegor/domain/model/Track.kt
+++ app/src/main/java/com/example/playlist_maker_android_rassohinegor/domain/model/Track.kt
@@ -3,5 +3,8 @@ package com.example.playlist_maker_android_rassohinegor.domain.model
 data class Track(
     val trackName: String,
     val artistName: String,
-    val trackTime: String
+    val trackTime: String,
+    val playlistId: Long = NO_PLAYLIST,
+    val id: Long,
+    val favorite: Boolean
 )
