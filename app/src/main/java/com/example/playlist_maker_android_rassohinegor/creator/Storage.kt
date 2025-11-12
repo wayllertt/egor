7df1cc7eb2adfe6b4d35b1683bedde45d68f640d
wayllertt <alidzhieva2005@mail.ru>
2025-11-12 21:00:01 +0300

    иьтkm

--- app/src/main/java/com/example/playlist_maker_android_rassohinegor/creator/Storage.kt
+++ app/src/main/java/com/example/playlist_maker_android_rassohinegor/creator/Storage.kt
@@ -7,52 +7,72 @@ class Storage {
         TrackDto(
             trackName = "Владивосток 2000",
             artistName = "Мумий Троль",
-            trackTimeMillis = 158_000
+            trackTimeMillis = 158_000,
+            id = 1,
+            favorite = false
         ),
         TrackDto(
             trackName = "Группа крови",
             artistName = "Кино",
-            trackTimeMillis = 283_000
+            trackTimeMillis = 283_000,
+            id = 2,
+            favorite = false
         ),
         TrackDto(
             trackName = "Не смотри назад",
             artistName = "Ария",
-            trackTimeMillis = 312_000
+            trackTimeMillis = 312_000,
+            id = 3,
+            favorite = false
         ),
         TrackDto(
             trackName = "Звезда по имени Солнце",
             artistName = "Кино",
-            trackTimeMillis = 225_000
+            trackTimeMillis = 225_000,
+            id = 4,
+            favorite = false
         ),
         TrackDto(
             trackName = "Лондон",
             artistName = "Аквариум",
-            trackTimeMillis = 272_000
+            trackTimeMillis = 272_000,
+            id = 5,
+            favorite = false
         ),
         TrackDto(
             trackName = "На заре",
             artistName = "Альянс",
-            trackTimeMillis = 230_000
+            trackTimeMillis = 230_000,
+            id = 6,
+            favorite = false
         ),
         TrackDto(
             trackName = "Перемен",
             artistName = "Кино",
-            trackTimeMillis = 296_000
+            trackTimeMillis = 296_000,
+            id = 7,
+            favorite = false
         ),
         TrackDto(
             trackName = "Розовый фламинго",
             artistName = "Сплин",
-            trackTimeMillis = 195_000
+            trackTimeMillis = 195_000,
+            id = 8,
+            favorite = false
         ),
         TrackDto(
             trackName = "Танцевать",
             artistName = "Мельница",
-            trackTimeMillis = 222_000
+            trackTimeMillis = 222_000,
+            id = 9,
+            favorite = false
         ),
         TrackDto(
             trackName = "Чёрный бумер",
             artistName = "Серега",
-            trackTimeMillis = 241_000
+            trackTimeMillis = 241_000,
+            id = 10,
+            favorite = false
         )
     )
 
