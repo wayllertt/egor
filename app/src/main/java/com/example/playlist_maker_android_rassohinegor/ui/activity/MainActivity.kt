5343ea00d34f470072b32c7edfca831cc9aef943
wayllertt <alidzhieva2005@mail.ru>
2025-11-20 22:06:18 +0300

    иьтkm

--- app/src/main/java/com/example/playlist_maker_android_rassohinegor/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/playlist_maker_android_rassohinegor/ui/activity/MainActivity.kt
@@ -1,13 +1,11 @@
-package com.example.playlist_maker_android_rassohinegor
+package com.example.playlist_maker_android_rassohinegor.ui.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.navigation.compose.rememberNavController
 import com.example.playlist_maker_android_rassohinegor.data.preferences.ThemePreferences
 import com.example.playlist_maker_android_rassohinegor.ui.navigation.PlaylistHost
